582c14ae3d371ea3e042af22e78ed745a02ee671
Vladislav Sotnikov <OSWVSotnikov@oriflame.com>
2022-02-07 10:57:24 +0100

    update: Update bem to mui - chats and paginator.

--- src/component/Chats/Chats.tsx
+++ src/component/Chats/Chats.tsx
@@ -1,83 +1,168 @@
+import { makeStyles } from "@mui/styles";
 import { NavLink } from "react-router-dom";
 
 import man from '../../assests/man.svg';
 import { withAuthRedirect } from '../../hoc/withAuthRedirect';
 
-const chatClassNames = 'chats__dialog-item';
-const activeChatClassName = 'chats__dialog-item--active';
-const setClassName = ({ isActive } : { isActive: boolean }) => isActive ? `${chatClassNames} ${activeChatClassName}`: chatClassNames;
+const useStyles = makeStyles({
+    chats: {
+        display: 'flex',
+    },
+
+    dialogs: {
+        width: '230px',
+    },
+
+    dialogItem: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '7px 10px',
+        borderRadius: '5px',
+        marginBottom: '5px',
+        transition: 'all .1s ease-in-out',
+
+        '&:hover': {
+            cursor: 'pointer',
+            backgroundColor: '#fff',
+        }
+    },
+
+    activeDialogItem: {
+        backgroundColor: '#4A76A8',
+        color: '#fff',
+
+        '&:hover': {
+            backgroundColor: '#4A76A8',
+            color: '#fff',
+        }
+    },
+
+    dialogPhoto: {
+        marginRight: '10px',
+    },
+
+    dialogName: {
+        fontSize: '12px',
+        fontWeight: 'bold',
+    },
+
+    dialogPreview: {
+        fontSize: '12px',
+    },
+
+    messages: {
+        marginLeft: '40px',
+    },
+
+    messageItem: {
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: '15px',
+    },
+
+    messagePhoto: {
+        marginRight: '15px',
+
+        '&>img': {
+            width: '30px',
+            height: '30px',
+        }
+    },
+
+    messageContent: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        minWidth: '200px',
+        backgroundColor: '#fff',
+        padding: '10px',
+        borderRadius: '5px',
+    },
+
+    messageTime: {
+        opacity: '.22',
+        marginLeft: '20px',
+    },
+
+    messageInput: {
+        border: '1px solid #DDDDDD',
+        padding: '10px 15px',
+        borderRadius: '5px',
+        minWidth: '300px',
+        marginRight: '30px',
+    }
+});
 
 export const Chats = withAuthRedirect(() => {
-    return (
-        <div className="chats">
-            <div className="chats__dialogs">
+    const classes = useStyles();
+    const setClassName = ({ isActive } : { isActive: boolean }) => isActive ? `${classes.dialogItem} ${classes.activeDialogItem}`: classes.dialogItem;
 
-                <ul className="chats__dialogs-list">
+    return (
+        <div className={classes.chats}>
+            <div className={classes.dialogs}>
+                <ul>
                     <NavLink to='/chats/1' className={setClassName}>
-                        <div className="chats__dialog-photo">
+                        <div className={classes.dialogPhoto}>
                             <img src={man} alt="man" />
                         </div>
-                        <div className="chats__dialog-content">
-                            <div className="chats__dialog-name">Vladyslav Sotnikov</div>
-                            <div className="chats__dialog-text">Some messages...</div>
+                        <div>
+                            <div className={classes.dialogName}>Vladyslav Sotnikov</div>
+                            <div className={classes.dialogPreview}>Some messages...</div>
                         </div>
                     </NavLink>
                     <NavLink to='/chats/2' className={setClassName}>
-                        <div className="chats__dialog-photo">
+                        <div className={classes.dialogPhoto}>
                             <img src={man} alt="man" />
                         </div>
-                        <div className="chats__dialog-content">
-                            <div className="chats__dialog-name">Vladyslav Sotnikov</div>
-                            <div className="chats__dialog-text">Some messages...</div>
+                        <div>
+                            <div className={classes.dialogName}>Vladyslav Sotnikov</div>
+                            <div className={classes.dialogPreview}>Some messages...</div>
                         </div>
                     </NavLink>
                     <NavLink to='/chats/3' className={setClassName}>
-                        <div className="chats__dialog-photo">
+                        <div className={classes.dialogPhoto}>
                             <img src={man} alt="man" />
                         </div>
-                        <div className="chats__dialog-content">
-                            <div className="chats__dialog-name">Vladyslav Sotnikov</div>
-                            <div className="chats__dialog-text">Some messages...</div>
+                        <div>
+                            <div className={classes.dialogName}>Vladyslav Sotnikov</div>
+                            <div className={classes.dialogPreview}>Some messages...</div>
                         </div>
                     </NavLink>
 
                 </ul>
             </div>
-            <div className="chats__messages">
-                <div className="chats__messages-content">
-                    <div className="chats__message-item">
-                        <div className="chats__message-photo">
+            <div className={classes.messages}>
+                <div>
+                    <div className={classes.messageItem}>
+                        <div className={classes.messagePhoto}>
                             <img src={man} alt="man" />
                         </div>
-                        <div className="chats__message-content">
-                            <p className="chats__message-text">Co słychać?</p>
-                            <span className="chats__message-time">17:55</span>
+                        <div className={classes.messageContent}>
+                            <p>Co słychać?</p>
+                            <span className={classes.messageTime}>17:55</span>
                         </div>
                     </div>
-                    <div className="chats__message-item">
-                        <div className="chats__message-photo">
+                    <div className={classes.messageItem}>
+                        <div className={classes.messagePhoto}>
                             <img src={man} alt="man" />
                         </div>
-                        <div className="chats__message-content">
-                            <p className="chats__message-text">Co słychać?</p>
-                            <span className="chats__message-time">17:55</span>
+                        <div className={classes.messageContent}>
+                            <p>Co słychać?</p>
+                            <span className={classes.messageTime}>17:55</span>
                         </div>
                     </div>
-                    <div className="chats__message-item">
-                        <div className="chats__message-photo">
+                    <div className={classes.messageItem}>
+                        <div className={classes.messagePhoto}>
                             <img src={man} alt="man" />
                         </div>
-                        <div className="chats__message-content">
-                            <p className="chats__message-text">I am junior React developer</p>
-                            <span className="chats__message-time">17:55</span>
+                        <div className={classes.messageContent}>
+                            <p>I am junior React developer</p>
+                            <span className={classes.messageTime}>17:57</span>
                         </div>
                     </div>
                 </div>
-                <div className="chats__message-form">
-                    <form>
-                        <input type="text" className="chats__message-input" placeholder="Wpisz wiadomość ..." />
-                        <button type="submit" className="chats__message-btn" > Odeśij</button>
-                    </form>
+                <div>
+                    <input type="text" className={classes.messageInput} placeholder="Wpisz wiadomość ..." />
+                    <button type="submit" className="chats__message-btn" > Odeśij</button>
                 </div>
             </div>
         </div>
